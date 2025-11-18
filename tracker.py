e73b22cc86efae89a7aede73a56b4c94ec7c99b0
isom3400a <7y6fsbpydh@privaterelay.appleid.com>
2025-11-18 15:26:24 +0800

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -4,3 +4,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 st.title("Personal Expense Tracker")
+
+if 'expenses' not in st.session_state:
+  st.session_state.expenses=pd.DataFrame(columns=['Date', 'Amount',"Description']
