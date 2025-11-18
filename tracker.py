6d989e59d937a6b7b89d57469f7bf12a0017c7b1
isom3400a <7y6fsbpydh@privaterelay.appleid.com>
2025-11-18 15:39:51 +0800

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -6,9 +6,22 @@ import numpy as np
 st.title("Personal Expense Tracker")
 
 if 'expenses' not in st.session_state:
-  st.session_state.expenses=pd.DataFrame(columns=['Date', 'Category','Amount','Description']
+  st.session_state.expenses=pd.DataFrame(columns=['Date', 'Category','Amount','Description'])
 
 with st.form("expense_form"):
   st.subheader("Add New Expense")
   date=st.date_input("Date")
-  
+  category=st.selectbox["Category", "Transport", "Entertainment", "Bills", "Others"])
+  amount=st.number_input("Amount", min_value=0.0, step=0.01)
+  description=st.text_input("Description")
+
+submitted=st.form_submit_button("Add Expense")
+if submitted:
+  new_expenses=pd.DataFrame({
+    'Date':[date],
+    'Category':[category],
+    'Amount':[amount],
+    'Description':[descripion]
+  })
+  st.session_state.expenses=pd.concat([st.session_state.expenses, new_expense], ignore_index=True)
+  st.success("Expense added successfully!")
