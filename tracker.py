430f4fcd0e585e4d9da3f19e3cdcfe386c636e86
isom3400a <7y6fsbpydh@privaterelay.appleid.com>
2025-11-18 15:30:01 +0800

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -6,4 +6,9 @@ import numpy as np
 st.title("Personal Expense Tracker")
 
 if 'expenses' not in st.session_state:
-  st.session_state.expenses=pd.DataFrame(columns=['Date', 'Amount',"Description']
+  st.session_state.expenses=pd.DataFrame(columns=['Date', 'Category','Amount','Description']
+
+with st.form("expense_form"):
+  st.subheader("Add New Expense")
+  date=st.date_input("Date")
+  
